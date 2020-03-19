04c6de2403b723611ecaf5b1e8e035518c17b3d4
Pawel Kolodziejczyk <pawel.kolodziejczyk@altimi.com>
2020-03-19 14:37:10 +0100

    add basic calendar view

---
+++ src/Calendar/Calendar.js
@@ -0,0 +1,99 @@
+import React, { Component } from 'react';
+import db_data from '../db/db_data.js';
+
+class Calendar extends Component {
+
+    days = ['Sun','Mon', 'Tue','Wen','Thu','Fri','Sat'];
+    months = ['January','February','March','April','May','Jun','July','August','September','October','November','December'];
+
+    state = {
+        current_day: new Date(),
+        
+    }
+
+    getReservationsForDate = (d) => {
+        var reservations = db_data
+            .filter(r => r.date_from.getDate() === d.getDate() && r.date_from.getMonth() === d.getMonth() + 1 && r.date_from.getFullYear() === d.getFullYear());
+        return reservations;
+    }
+
+    nextWeek = () => {
+        var nextDate = new Date(this.state.current_day);
+        console.log(nextDate);
+        this.setState({ current_day: nextDate.setDate(nextDate.getDate() + 7) });
+    }
+
+    previousWeek = () => {
+        var nextDate = new Date(this.state.current_day);
+        this.setState({ current_day: nextDate.setDate(nextDate.getDate() - 7) });
+    }
+
+    renderCalendar = (startDate) => {
+        let current_date = this.getMonday(startDate === undefined ? new Date() : startDate);
+        let days = [];
+
+        while (days.length < 7) {
+            var callendar_day = new Date(current_date);
+            days.push(callendar_day);
+            current_date.setDate(current_date.getDate() + 1);
+        }
+
+        let calendar = (
+            <div className="CalendarWrapper">
+                <div className="CalendarControls">
+                    <button id="btnPrevious" onClick={this.previousWeek}>Previous</button>
+                    <button className="CurrentMonth">
+                    {this.months[current_date.getMonth()]}
+                    </button>
+                    <button id="btnNext" onClick={this.nextWeek}>Next</button>
+                </div>
+
+
+                <div className="Calendar">
+                    {
+                        days.map(d => {
+                            let reservations = this.getReservationsForDate(d);
+
+                            return (
+                                <div className="Day">
+                                     {this.days[d.getDay()]} {d.getDate()}
+                                    <div className="Reservations">
+                                        {
+                                            reservations.length !== 0 || reservations.length !== undefined ?
+                                                reservations.map(r => {
+                                                    return (
+                                                    <div>
+                                                        {r.date_from.getHours()}:{r.date_from.getMinutes() < 10 ? r.date_from.getMinutes().toString() + '0' :r.date_from.getMinutes() } 
+                                                         - 
+                                                        {r.date_to.getHours()}:{r.date_to.getMinutes() < 10 ? r.date_to.getMinutes().toString() + '0': r.date_to.getMinutes() } 
+                                                    </div>)
+                                                }) : ""
+                                        }
+                                    </div>
+                                </div>);
+                        })
+                    }
+                </div>
+            </div>
+        )
+
+        return calendar;
+    }
+
+    getMonday(d) {
+        d = new Date(d);
+        var day = d.getDay(),
+            diff = d.getDate() - day + (day == 0 ? -6 : 1);
+        return new Date(d.setDate(diff));
+    }
+
+    render = () => {
+        return (
+            <div>
+                {this.renderCalendar(this.state.current_day)}
+            </div>
+        );
+    }
+}
+
+export default Calendar;
---
+++ src/db/db_data.js
@@ -0,0 +1,44 @@
+
+
+
+let reservations =
+    [
+        {
+            // day: 19,
+            // month: 3,
+            // year: 2020,
+            date_from: new Date(2020,3,19,16,0),
+            date_to: new Date(2020,3,19,16,15),
+            // hour_from: "16:00",
+            // hour_to: "16:15",
+            name: "Paweł Kołodziejczyk",
+            phone: "781531306",
+            email: "p.kolodziej92@gmail.com"
+        },
+        {
+            // day: 20,
+            // month: 3,
+            // year: 2020,
+            date_from: new Date(2020,3,19,12,0),
+            date_to: new Date(2020,3,19,12,15),
+            // hour_from: "12:00",
+            // hour_to: "12:15",
+            name: "Jan Kowalski",
+            phone: "34324234",
+            email: "kowalski@gmail.com"
+        },
+        {
+            // day: 19,
+            // month: 3,
+            // year: 2020,
+            date_from: new Date(2020,3,20,16,30),
+            date_to: new Date(2020,3,20,16,45),
+            // hour_from: "16:30",
+            // hour_to: "16:45",
+            name: "Jarek Karczewski",
+            phone: "12312312",
+            email: "jar.kar@gmail.com"
+        },
+    ];
+
+    export default reservations;
