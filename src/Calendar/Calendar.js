14cdfe19fc1135f36c003f81f83f5ce5e7280c57
Pawel Kolodziejczyk <pawel.kolodziejczyk@altimi.com>
2020-03-20 13:53:29 +0100

    add date selection functionality, add reservation form

--- src/Calendar/Calendar.js
+++ src/Calendar/Calendar.js
@@ -1,35 +1,63 @@
 import React, { Component } from 'react';
-import db_data from '../db/db_data.js';
+import { availableAppointments, reservations } from '../db/db_data.js';
 
 class Calendar extends Component {
 
-    days = ['Sun','Mon', 'Tue','Wen','Thu','Fri','Sat'];
-    months = ['January','February','March','April','May','Jun','July','August','September','October','November','December'];
+    days = ['Sun', 'Mon', 'Tue', 'Wen', 'Thu', 'Fri', 'Sat'];
+    months = ['January', 'February', 'March', 'April', 'May', 'Jun', 'July', 'August', 'September', 'October', 'November', 'December'];
 
     state = {
-        current_day: new Date(),        
+        current_day: new Date(),
+        selected_appointmentId: undefined,
+        selected_appointment_date: undefined,
     }
 
     getReservationsForDate = (d) => {
-        var reservations = db_data
+        var reservations = availableAppointments
             .filter(r => r.date_from.getDate() === d.getDate() && r.date_from.getMonth() === d.getMonth() + 1 && r.date_from.getFullYear() === d.getFullYear())
-            
+
         return reservations.sort((date1, date2) => {
             if (date1.date_from > date2.date_from) return 1;
             if (date1.date_from < date2.date_from) return -1;
-            return 0;});
+            return 0;
+        });
+    }
+
+    getAppointmentDate = (id) => {
+        let appointment = availableAppointments.find(a => a.id == id);
+
+        return appointment;
+
+    }
+
+    selectAppointmentWindow = (e) => {
+        if(e.target.classList.contains("Reserved")) return;
+        let appointmentId = e.target.getAttribute("data-appointment-id");
+        this.setState({ selected_appointmentId: appointmentId });
+       
+        let appointment = this.getAppointmentDate(appointmentId);
+        this.setState({ selected_appointment_date: appointment.date_from.toLocaleString() });
+        // change element style to show that its reserved
+    }
+
+    restSelectedAppointments = () => {
+        this.setState({selected_appointmentId: undefined});
+        this.setState({selected_appointment_date: undefined});
     }
 
     nextWeek = () => {
         var nextDate = new Date(this.state.current_day);
-        console.log(nextDate);
         this.setState({ current_day: nextDate.setDate(nextDate.getDate() + 7) });
+        this.restSelectedAppointments();
     }
 
     previousWeek = () => {
         var nextDate = new Date(this.state.current_day);
         this.setState({ current_day: nextDate.setDate(nextDate.getDate() - 7) });
+        this.restSelectedAppointments();
     }
+
+
 
     renderCalendar = (startDate) => {
         let current_date = this.getMonday(startDate === undefined ? new Date() : startDate);
@@ -46,7 +74,7 @@ class Calendar extends Component {
                 <div className="CalendarControls">
                     <button id="btnPrevious" onClick={this.previousWeek}>Previous</button>
                     <button className="CurrentMonth">
-                    {this.months[current_date.getMonth()]}
+                        {this.months[current_date.getMonth()]}
                     </button>
                     <button id="btnNext" onClick={this.nextWeek}>Next</button>
                 </div>
@@ -56,20 +84,19 @@ class Calendar extends Component {
                         days.map(d => {
                             let reservations = this.getReservationsForDate(d);
 
-        
                             return (
-                                <div className={`Day ${this.isToday(d) ? "CurrentDay" : "" }`}>
-                                     {this.days[d.getDay()]} {d.getDate()}
+                                <div className={`Day ${this.isToday(d) ? "CurrentDay" : ""}`}>
+                                    {this.days[d.getDay()]} {d.getDate()}
                                     <div className="Reservations">
                                         {
                                             reservations.length !== 0 || reservations.length !== undefined ?
-                                                reservations.map(r => {
+                                                reservations.map(a => {
                                                     return (
-                                                    <div>
-                                                        {r.date_from.getHours()}:{r.date_from.getMinutes() < 10 ? r.date_from.getMinutes().toString() + '0' :r.date_from.getMinutes() } 
-                                                         - 
-                                                        {r.date_to.getHours()}:{r.date_to.getMinutes() < 10 ? r.date_to.getMinutes().toString() + '0': r.date_to.getMinutes() } 
-                                                    </div>)
+                                                        <div className={`AppointmentSlot ${a.reservation_id != undefined ? "Reserved": ""} ${a.id == this.state.selected_appointmentId ? "Selected": ""}`} data-appointment-id={`${a.id}`} onClick={this.selectAppointmentWindow}>
+                                                            {a.date_from.getHours()}:{a.date_from.getMinutes() < 10 ? a.date_from.getMinutes().toString() + '0' : a.date_from.getMinutes()}
+                                                         -
+                                                            {a.date_to.getHours()}:{a.date_to.getMinutes() < 10 ? a.date_to.getMinutes().toString() + '0' : a.date_to.getMinutes()}
+                                                        </div>)
                                                 }) : ""
                                         }
                                     </div>
@@ -77,6 +104,33 @@ class Calendar extends Component {
                         })
                     }
                 </div>
+
+                    
+                    <form className={`ReservationForm ${this.state.selected_appointmentId == undefined ? "Hidden" : ""}`} >
+                        <button className="ReservationFormClose" type="button" onClick={this.restSelectedAppointments}>X</button>
+                        <h3>Reservation</h3>
+
+                        <input hidden={true} name="appointment_id" value={this.state.selected_appointmentId}></input>
+
+                        <div>
+                            <label for="selectedDate">Date</label>
+                            <div id="selectedAppointmentDate">{this.state.selected_appointment_date != undefined ? this.state.selected_appointment_date : ""}</div>
+                        </div>
+                        <div>
+                            <label for="name">Name</label>
+                            <input name="name" type="text"></input>
+                        </div>
+                        <div>
+                            <label for="email">Email</label>
+                            <input name="email" type="text"></input>
+                        </div>
+                        <div>
+                            <label for="phone">Phone</label>
+                            <input name="phone" type="text" ></input>
+                        </div>
+                        <button type="button">Make reservation</button>
+                    </form>
+
             </div>
         )
 
@@ -90,12 +144,12 @@ class Calendar extends Component {
         return new Date(d.setDate(diff));
     }
 
-     isToday = (someDate) => {
+    isToday = (someDate) => {
         const today = new Date()
         return someDate.getDate() == today.getDate() &&
-          someDate.getMonth() == today.getMonth() &&
-          someDate.getFullYear() == today.getFullYear()
-      }
+            someDate.getMonth() == today.getMonth() &&
+            someDate.getFullYear() == today.getFullYear()
+    }
 
     render = () => {
         return (
--- src/db/db_data.js
+++ src/db/db_data.js
@@ -1,12 +1,50 @@
 
+export let availableAppointments= [
+    {
+        id:1,
+        date_from: new Date(2020,3,19,8,0),
+        date_to: new Date(2020,3,19,8,15),
+        reservation_id: 1, 
+    },
+    {
+        id:2,
+        date_from: new Date(2020,3,19,8,15),
+        date_to: new Date(2020,3,19,8,30),
+        reservation_id: null, 
+    },
+    {
+        id:3,
+        date_from: new Date(2020,3,19,8,30),
+        date_to: new Date(2020,3,19,8,45),
+        reservation_id: null, 
+    },
+    {
+        id:4,
+        date_from: new Date(2020,3,19,8,45),
+        date_to: new Date(2020,3,19,9,0),
+        reservation_id: 2, 
+    },
+    {
+        id:5,
+        date_from: new Date(2020,3,19,10,0),
+        date_to: new Date(2020,3,19,10,15),
+        reservation_id: null, 
+    }
+]
 
 
-let reservations =
+
+
+
+
+
+export let reservations =
     [
         {
             // day: 19,
             // month: 3,
             // year: 2020,
+            id: 1,
             date_from: new Date(2020,3,19,16,0),
             date_to: new Date(2020,3,19,16,15),
             // hour_from: "16:00",
@@ -16,65 +54,45 @@ let reservations =
             email: "p.kolodziej92@gmail.com"
         },
         {
-            // day: 20,
-            // month: 3,
-            // year: 2020,
+            id:2,
             date_from: new Date(2020,3,19,12,0),
             date_to: new Date(2020,3,19,12,15),
-            // hour_from: "12:00",
-            // hour_to: "12:15",
             name: "Jan Kowalski",
             phone: "34324234",
             email: "kowalski@gmail.com"
         },
         {
-            // day: 19,
-            // month: 3,
-            // year: 2020,
+            id:3,
             date_from: new Date(2020,3,20,16,30),
             date_to: new Date(2020,3,20,16,45),
-            // hour_from: "16:30",
-            // hour_to: "16:45",
             name: "John Doe",
             phone: "12312312",
             email: "john.doe@gmail.com"
         },
         {
-            // day: 19,
-            // month: 3,
-            // year: 2020,
+            id:4,
             date_from: new Date(2020,3,20,8,30),
             date_to: new Date(2020,3,20,8,45),
-            // hour_from: "16:30",
-            // hour_to: "16:45",
             name: "John Doe",
             phone: "12312312",
             email: "john.doe@gmail.com"
         },
         {
-            // day: 19,
-            // month: 3,
-            // year: 2020,
+            id:5,
             date_from: new Date(2020,3,16,18,30),
             date_to: new Date(2020,3,16,18,45),
-            // hour_from: "16:30",
-            // hour_to: "16:45",
             name: "John Doe",
             phone: "12312312",
             email: "john.doe@gmail.com"
         },
         {
-            // day: 19,
-            // month: 3,
-            // year: 2020,
+            id:6,
             date_from: new Date(2020,3,13,16,30),
             date_to: new Date(2020,3,13,16,45),
-            // hour_from: "16:30",
-            // hour_to: "16:45",
             name: "John Doe",
             phone: "12312312",
             email: "john.doe@gmail.com"
         },
     ];
 
-    export default reservations;
+    export default availableAppointments;
