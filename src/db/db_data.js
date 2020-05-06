d094fe1a911b699a0360e2dffdce10d7d4ca6e22
Pawel Kolodziejczyk <pawel.kolodziejczyk@altimi.com>
2020-05-06 13:53:26 +0200

    dummy data cleanup

--- src/db/db_data.js
+++ src/db/db_data.js
@@ -29,14 +29,25 @@ export let availableAppointments= [
         date_from: new Date(2020,3,19,10,0),
         date_to: new Date(2020,3,19,10,15),
         reservation_id: null, 
+    },
+    {
+        id:6,
+        date_from: new Date(2020,3,20,10,0),
+        date_to: new Date(2020,3,20,10,15),
+        reservation_id: null, 
+    },
+    {
+        id:7,
+        date_from: new Date(2020,3,20,7,30),
+        date_to: new Date(2020,3,19,7,45),
+        reservation_id: null, 
+    },    {
+        id:8,
+        date_from: new Date(2020,3,20,7,45),
+        date_to: new Date(2020,3,19,8,0),
+        reservation_id: null, 
     }
 ]
-
-
-
-
-
-
 
 export let reservations =
     [
@@ -45,8 +56,8 @@ export let reservations =
             // month: 3,
             // year: 2020,
             id: 1,
-            date_from: new Date(2020,3,19,16,0),
-            date_to: new Date(2020,3,19,16,15),
+            // date_from: new Date(2020,3,19,16,0),
+            // date_to: new Date(2020,3,19,16,15),
             // hour_from: "16:00",
             // hour_to: "16:15",
             name: "Paweł Kołodziejczyk",
@@ -55,40 +66,30 @@ export let reservations =
         },
         {
             id:2,
-            date_from: new Date(2020,3,19,12,0),
-            date_to: new Date(2020,3,19,12,15),
             name: "Jan Kowalski",
             phone: "34324234",
             email: "kowalski@gmail.com"
         },
         {
             id:3,
-            date_from: new Date(2020,3,20,16,30),
-            date_to: new Date(2020,3,20,16,45),
             name: "John Doe",
             phone: "12312312",
             email: "john.doe@gmail.com"
         },
         {
             id:4,
-            date_from: new Date(2020,3,20,8,30),
-            date_to: new Date(2020,3,20,8,45),
             name: "John Doe",
             phone: "12312312",
             email: "john.doe@gmail.com"
         },
         {
             id:5,
-            date_from: new Date(2020,3,16,18,30),
-            date_to: new Date(2020,3,16,18,45),
             name: "John Doe",
             phone: "12312312",
             email: "john.doe@gmail.com"
         },
         {
             id:6,
-            date_from: new Date(2020,3,13,16,30),
-            date_to: new Date(2020,3,13,16,45),
             name: "John Doe",
             phone: "12312312",
             email: "john.doe@gmail.com"
