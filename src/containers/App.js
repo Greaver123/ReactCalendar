370c7918952049c529affe38b0b5f8a9d3d68731
Pawel Kolodziejczyk <pawel.kolodziejczyk@altimi.com>
2020-05-20 12:10:03 +0200

    add basic routes

--- src/containers/App.js
+++ src/containers/App.js
@@ -1,12 +1,18 @@
 import React, { Component } from 'react';
 import './App.css';
 import Calendar from '../components/Calendar/Calendar.js';
+import { Route, Link, Switch } from 'react-router-dom';
 
 class App extends Component {
   render = () => {
     return (
       <div>
-        <Calendar/>
+        <Switch>
+          <Route path="/appointments" exact component={Calendar} />
+          <Route path="/appointments/create" exact render={() => { return (<p>Create apointment</p>) }} />
+          <Route path="/about" render={() => { return (<p>About me</p>) }} />
+          <Route path="/" render={() => { return (<p>Home</p>) }} />
+        </Switch>
       </div>
     );
   }
